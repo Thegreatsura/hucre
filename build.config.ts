736428c4945bb0f00be4d0d0b3da48311092e54e
productdevbook <hi@productdevbook.com>
2026-03-24 21:22:17 +0300

    chore: initial project setup

---
+++ build.config.ts
@@ -0,0 +1,16 @@
+import { defineBuildConfig } from "obuild/config";
+
+export default defineBuildConfig({
+  entries: [
+    {
+      type: "bundle",
+      input: [
+        "./src/index.ts",
+        "./src/xlsx.ts",
+        "./src/csv.ts",
+        "./src/ods.ts",
+      ],
+      minify: true,
+    },
+  ],
+});
